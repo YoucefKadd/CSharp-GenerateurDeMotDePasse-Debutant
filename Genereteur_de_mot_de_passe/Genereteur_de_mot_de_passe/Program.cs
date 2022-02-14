d57b5504bfcd38f536e764a6b1724e76d597b4b3
Youcef k <youssefkaddour@hotmail.com>
2022-02-14 23:54:48 +0100

    ajout de k'alphabet miniscules

--- Genereteur_de_mot_de_passe/Genereteur_de_mot_de_passe/Program.cs
+++ Genereteur_de_mot_de_passe/Genereteur_de_mot_de_passe/Program.cs
@@ -7,7 +7,19 @@ namespace Generateur_de_mo_de_passe // Note: actual namespace depends on the pro
     {
         static void Main(string[] args)
         {
-           int longueurMotDePasse = Outils.DemanderNombre("Longueur du mot de passe :");
+            //int longueurMotDePasse = Outils.DemanderNombre("Longueur du mot de passe :");
+
+            string miniscules = "azertyuiopqsdfghjklmwxcvbn";
+            string alphabet = miniscules;
+
+            int l = alphabet.Length;  
+
+            
+            Random rand = new Random();
+            int index = rand.Next(0, l);
+            Console.WriteLine(alphabet[index]);
+
+
         }
     }
 }
