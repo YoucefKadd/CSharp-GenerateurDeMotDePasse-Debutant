140907e438cf1ecb5e2feb717c4498a249979dbd
Youcef k <youssefkaddour@hotmail.com>
2022-02-15 00:32:51 +0100

    Choix de la complexité de l alphabet par l'utilisateur

--- Genereteur_de_mot_de_passe/Genereteur_de_mot_de_passe/Program.cs
+++ Genereteur_de_mot_de_passe/Genereteur_de_mot_de_passe/Program.cs
@@ -8,15 +8,52 @@ namespace Generateur_de_mo_de_passe // Note: actual namespace depends on the pro
         static void Main(string[] args)
         {
             int longueurMotDePasse = Outils.DemanderNombre("Longueur du mot de passe :");
+            Console.WriteLine();
+
+
+            // Vous voulez un mot de passe avec :
+            // 1 - uniquement des caractere en miniscule
+            // 2 - des caracteres miniscule et majuscule
+            // 3 - des caractere et des chiffres
+            // 4 - caracteres, chiffres et carctere spéciaux
+
+            int choixAlphabet = Outils.DemanderNombreEntre("Vous voulez un mot de passe avec :\n" +
+                "1 - uniquement des caractere en miniscule\n" +
+                "2 - des caracteres miniscule et majuscule\n" +
+                "3 - des caractere et des chiffres\n" +
+                "4 - caracteres, chiffres et carctere spéciaux\n" +
+                $"Votre choix :      ", 1, 4);
 
             string miniscules = "azertyuiopqsdfghjklmwxcvbn";
             string majuscules = miniscules.ToUpper();
             string chiffres = "0123456789";
             string caracteresSpeciaux = "&#+-.";
-            string alphabet = miniscules + majuscules + chiffres + caracteresSpeciaux;
+            string alphabet= "";
             string motDePasse = "";
-            int longueurAlphabet = alphabet.Length;
             Random rand = new Random();
+
+            if (choixAlphabet == 1)
+            {
+                alphabet = miniscules;
+            }
+            else if (choixAlphabet == 2)
+            {
+                alphabet = miniscules + majuscules;
+            }
+            else if (choixAlphabet == 3)
+            {
+                alphabet = miniscules + majuscules + chiffres;
+            }
+            else if (choixAlphabet == 4)
+            {
+                alphabet = miniscules + majuscules + chiffres + caracteresSpeciaux;
+            }
+
+
+
+            int longueurAlphabet = alphabet.Length;
+
+
 
 
             for (int i = 0; i < longueurMotDePasse; i++)
