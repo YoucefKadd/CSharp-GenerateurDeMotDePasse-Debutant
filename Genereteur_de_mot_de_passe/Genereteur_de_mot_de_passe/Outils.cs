0f197c884c0a0eb2bfebf0c13a32c0b2d0e8ffd1
Youcef k <youssefkaddour@hotmail.com>
2022-02-15 01:00:00 +0100

    Generations de plusieurs mot de passe

--- Genereteur_de_mot_de_passe/Genereteur_de_mot_de_passe/Outils.cs
+++ Genereteur_de_mot_de_passe/Genereteur_de_mot_de_passe/Outils.cs
@@ -10,10 +10,10 @@ namespace FormationCS
     {
         public static int DemanderNombrePositifNonNul(string question)
         {
-            return DemanderNombreEntre(question, 1, int.MaxValue);
+            return DemanderNombreEntre(question, 1, int.MaxValue, "Erreur : le nombre doit etre positifet non nul");
         }
-        public static int DemanderNombreEntre(string question, int min, int max)
-        {
+        public static int DemanderNombreEntre(string question, int min, int max, string messageErreurPersonalise = null)
+        { // le string messageErreurPersonalise = null permet d'indiquer que c'est optionnel
 
 
             int nombre = DemanderNombre(question);
@@ -21,7 +21,15 @@ namespace FormationCS
             {
                 return nombre;
             }
-            Console.WriteLine($"Erreur : le nombre doit être compris entre {min} et {max}");
+            if (messageErreurPersonalise == null)
+            {
+                Console.WriteLine($"Erreur : le nombre doit être compris entre {min} et {max}");
+            }
+            else
+            {
+                Console.WriteLine(messageErreurPersonalise); 
+            }
+            Console.WriteLine();
             return DemanderNombreEntre(question, min, max);
         }
         public static int DemanderNombre(String question)
@@ -39,6 +47,7 @@ namespace FormationCS
                 catch
                 {
                     Console.WriteLine("Erreur : vous devez rentrer un nombre !");
+                    Console.WriteLine();
                 }
             }
         }
